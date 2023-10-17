007968d377b9c85f96be091dab2b893509c8d4ae
Ufuoma Oribabor <ufo@Ufuomas-MacBook-Air.local>
2023-10-17 01:28:34 +0100

    first commit

---
+++ Random-function-tests .py
@@ -0,0 +1,35 @@
+#Learning how to make the mood guessing game
+
+#import random
+
+#mood=(0)
+
+#print ("Hey there, i can guess how you feel")
+
+#if mood == 1:
+  #print ("I can tell you are happy")
+
+#elif mood ==2:
+  #print ("I can tell you are sad")
+
+#elif mood ==3 :
+ # print ("I can tell you are angry")
+
+#else:
+ # print ("I can't even tell how you feel")
+
+#input("\n\n Press the enter key to exit the program")
+
+
+#The three year old simulator program
+
+print ("Welcome to the three year old simmulator program")
+print ("This program simulates a conversation with a three year old child")
+print ("Try to stop this madness")
+
+response = ""
+while response != "Because,":
+    response = input ("Why?\n")
+
+print ("Oh okay.")
+input ("Press the enter key to exit")
